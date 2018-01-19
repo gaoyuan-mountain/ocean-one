9998e7e34d719e4ad3bf2911266245a120bda5d6
高原 <yuan.gao@dianrong.com>
2018-01-19 11:27:20 +0800

    修改注释

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function hashPrefixExcept(prefix) {
 }
 
 // TIPS: 这里用到了import()，是ecmascript stage3定义的方法，作用是动态import，返回promise。配合babel的话，需要babel-plugin-syntax-dynamic-import
-// TIPS: declareChildApplication支持两种写法，当两个参数时，参数1代表appName，参数2代表生效路由前缀，加载器会默认使用import()。三个参数时，参数1代表appName，参数2代表加载器，参数3代表生效路由前缀
+// TIPS: registerApplication支持两种写法，当两个参数时，参数1代表appName，参数2代表生效路由前缀，加载器会默认使用import()。三个参数时，参数1代表appName，参数2代表加载器，参数3代表生效路由前缀
 registerApplication('menu', () => import(/* webpackChunkName: "menu" */ './menu/menu.ocean.js'), hashPrefixExcept('/signin'));
 registerApplication('toolbar', () => import(/* webpackChunkName: "toolbar" */ './toolbar/toolbar.ocean.js'), hashPrefixExcept('/signin'));
 registerApplication('activity', () => import(/* webpackChunkName: "activity" */'./activity/activity.ocean.js'), hashPrefix('/activity'));
